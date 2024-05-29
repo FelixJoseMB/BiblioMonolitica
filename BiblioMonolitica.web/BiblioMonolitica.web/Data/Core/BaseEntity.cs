d2237be20f938a91f53212cb28fe0834e9c61ee8
DalbertSanchez <sdalbert34@gmail.com>
2024-05-29 16:27:14 -0400

    Conclusion de las entidades EstadoPrestamo y Usuarios

--- BiblioMonolitica.web/BiblioMonolitica.web/Data/Core/BaseEntity.cs
+++ BiblioMonolitica.web/BiblioMonolitica.web/Data/Core/BaseEntity.cs
@@ -2,18 +2,23 @@
 {
     public abstract class BaseEntity
     {
-        public bool Estado { get; set; }
+        public bool? Estado { get; set; }
 
-        public DateTime FechaCreacion { get; set; }
+        public DateTime? FechaCreacion { get; set; }
 
 
-        public required string Descripcion { get; set; }
+        public required string? Descripcion { get; set; }
 
-        public int Codigo { get; set; }
+        public int? Codigo { get; set; }
 
-        public required string Clave { get; set; }
+        public required string? Clave { get; set; }
 
         public required string Correo { get; set; }
+
+        protected BaseEntity() 
+        {
+            this.FechaCreacion = DateTime.Now;
+        }
 
 
 
